9147ce59623b5ccf937d3fe8703fbe726d762a95
Sdm046 <156705391+Sdm046@users.noreply.github.com>
2025-07-24 22:36:56 -0400

    Add files via upload

---
+++ web-development/user_input_project.js
@@ -0,0 +1,38 @@
+//let a = process.argv[3];
+//console.log(a);
+
+//let a = process.argv[3];
+//console.log(a);
+
+
+
+
+
+//console.log(ProcessingInstruction.argv[2]);
+//console.log(ProcessingInstruction.argv[3]);
+//  console.log(process.argv[4]);
+
+
+
+
+
+
+//console.log(typeof(input1));
+//console.log(typeof(input2));
+
+
+
+
+
+
+const [Name, Age, Gender, Birthday] = process.argv.slice(2);
+
+console.log("Name: " + a[2]);
+
+console.log("Age: " + a[3]);
+
+console.log("Gender: " + a[4]);
+
+console.log("Birthday: " + a[5]);
+
+
